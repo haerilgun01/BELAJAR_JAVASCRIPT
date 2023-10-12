5096133beccd5a1f50b2397f82f832d5c9e4ac92
haeril gunadi <144421938+haerilgun01@users.noreply.github.com>
2023-10-12 21:17:54 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+// mengubah teks menjasi UpperCase
+let teks1 = "merah putih"
+let teks2 = "merah muda"
+let uppercase = teks1.toUpperCase()
+
+// mengubah teks menjadi LowerCase
+let lowercase = teks2.toLowerCase()
+
+// variabel mengukur panjang(length)
+let teks12 = "merah putih"
+let ubahTeks = teks12.replace ("putih", "muda")
+
+// variabel mengukur panjang (length)
+let teks11 = "merah putih"
+let panjangteks = teks11.length
+
+// mengambil output
+alert(uppercase)
+alert(lowercase)
+alert(ubahTeks)
+alert(panjangteks)
